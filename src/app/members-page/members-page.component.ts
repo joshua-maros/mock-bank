8fbab478af39aab93266d9b5136e1b5f8971f0d6
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2018-10-06 12:37:43 -0700

    Aesthetic improvements for member page.

--- src/app/members-page/members-page.component.ts
+++ src/app/members-page/members-page.component.ts
@@ -10,7 +10,13 @@ export class MembersPageComponent implements OnInit {
   members: Member[];
 
   constructor(private backend: WebappBackendService) {
-    backend.getCachedMemberList().then(e => this.members = e);
+    backend.getCachedMemberList().then(e => this.members = e.sort((a, b) => {
+      if (a.firstName === b.firstName) {
+        return (a.lastName > b.lastName ? -1 : 1);
+      } else {
+        return (a.firstName > b.firstName ? -1 : 1);
+      }
+    }).filter(f => f.firstName !== '!'));
   }
 
   ngOnInit() { }
