065c9c99991e06ffa2466bb144ff27f4975c1b6c
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2018-10-05 18:11:48 -0700

    Cleaned up main app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'extensive-versatile-investment-log';
