ec8f8e2ee526ce146feb6e81bb27c223b11afee7
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2018-10-13 14:18:37 -0700

    Members can be clicked on to get to their detailed pages.

--- src/app/members-page/members-page.component.ts
+++ src/app/members-page/members-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Member, WebappBackendService } from '../webapp-backend.service';
 import { sortMembers } from '../util';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-members-page',
@@ -10,8 +11,12 @@ import { sortMembers } from '../util';
 export class MembersPageComponent implements OnInit {
   members: Member[];
 
-  constructor(private backend: WebappBackendService) {
+  constructor(private backend: WebappBackendService, private router: Router) {
     backend.getCachedMemberList().then(e => this.members = sortMembers(e, true));
+  }
+
+  gotoMember(member: Member) {
+    this.router.navigateByUrl('/members/' + member.id);
   }
 
   ngOnInit() { }
