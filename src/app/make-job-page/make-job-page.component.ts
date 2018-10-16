b2992c7b53ef979d44fc469975ea212256833475
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2018-10-16 14:14:29 -0700

    make-job-page now uses a fade hint.

--- src/app/make-job-page/make-job-page.component.ts
+++ src/app/make-job-page/make-job-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { WebappBackendService, Member, AccessLevel, Class } from '../webapp-backend.service';
 import { FormBuilder, Validators } from '@angular/forms';
 import { sortMembers } from '../util';
+import { FadeHintComponent } from '../fade-hint/fade-hint.component';
 
 @Component({
   selector: 'app-make-job-page',
@@ -10,6 +11,8 @@ import { sortMembers } from '../util';
 })
 export class MakeJobPageComponent implements OnInit {
   public members: Member[];
+  private quickAddList: Member[] = [];
+  @ViewChild('hint') hint: FadeHintComponent;
 
   @ViewChild('form') form;
   fg = this.fb.group({
@@ -18,7 +21,6 @@ export class MakeJobPageComponent implements OnInit {
     orangeSalary: [0, Validators.required],
     toAdd: [null]
   });
-  quickAddList: Member[] = [];
 
   constructor(private backend: WebappBackendService, private fb: FormBuilder) { 
     this.backend.getCachedMemberList().then(e => this.members = sortMembers(e, true));
@@ -39,15 +41,22 @@ export class MakeJobPageComponent implements OnInit {
     const v = this.fg.value;
     this.fg.disable();
     (async () => {
-      let res = await this.backend.createJob(v.name, v.blueSalary, v.orangeSalary);
-      if (res.ok) {
-        for (const member of this.quickAddList) {
-          await this.backend.patchMember(member, {
-            jobs: member.jobs.concat(res.body.id)
-          });
+      try {
+        let res = await this.backend.createJob(v.name, v.blueSalary, v.orangeSalary);
+        if (res.ok) {
+          for (const member of this.quickAddList) {
+            await this.backend.patchMember(member, {
+              jobs: member.jobs.concat(res.body.id)
+            });
+          }
+          this.quickAddList = [];
+          this.form.resetForm();
+          this.hint.showMessage('Job created!');
+        } else {
+          this.hint.showError('Error encountered while creating job.');
         }
-        this.quickAddList = [];
-        this.form.resetForm()
+      } catch {
+        this.hint.showError('Error encountered while creating job.');
       }
       this.members = sortMembers(await this.backend.getCachedMemberList(), true);
       this.fg.enable();
