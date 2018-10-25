209498140201d0f45e41f5b6106a95d7e8109efb
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2018-10-24 23:16:18 -0700

    Shows different stuff on homepage for users and for leaders.

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -7,6 +7,14 @@ import { WebappBackendService, AccessLevel } from '../webapp-backend.service';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+  get showMemberLinks() {
+    return this.backend.getAccessLevel() === AccessLevel.MEMBER;
+  }
+
+  get leader() {
+    return this.backend.getAccessLevel() === AccessLevel.LEADER;
+  }
+
   constructor(private backend: WebappBackendService) { }
 
   ngOnInit() { }
